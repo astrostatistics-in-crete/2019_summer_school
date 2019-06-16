a379d10ceee0e688e42ae0762413351d4ea90b46
Konstantinos Kovlakas <kkovlakas@physics.uoc.gr>
2019-06-16 20:28:46 +0300

    Updated 'get_data.py'

--- get_data.py
+++ get_data.py
@@ -3,12 +3,13 @@ import requests
 
 parent = "http://astro.physics.uoc.gr/Conferences/Astrostatistics_School_Crete_2019/data/"
 datafiles_name = "datafiles"
+setupdata_name = "setup_data.sh"
 
 for filepath in requests.get(parent + datafiles_name).text.split():
     if not filepath.startswith("./"):
         raise Exception("Unexpected entry of datafiles.")
     filepath = filepath[2:]
-    if filepath != datafiles_name:
+    if filepath != datafiles_name and filepath != setupdata_name:
         folder, filename = os.path.split(filepath)
         local_folder = os.path.join(folder, "data")
         local_filename = os.path.join(local_folder, filename)
